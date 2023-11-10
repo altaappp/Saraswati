3c5daac5b5ac13df34d69bee5777d91b6e34b8a5
Altaff <121285212+altaappp@users.noreply.github.com>
2023-11-10 20:57:27 +0700

    Update Penjual.java

--- Saraswati/src/saraswati/Penjual.java
+++ Saraswati/src/saraswati/Penjual.java
@@ -2,4 +2,43 @@ package saraswati;
 public class Penjual {
     private int id;
     private String firstName, lastName, email, jobTitle;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
 }
