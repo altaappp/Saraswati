871264add5b01761cf6fb18ee1d58bd703e9e63f
Altaff <121285212+altaappp@users.noreply.github.com>
2023-11-10 20:58:13 +0700

    Update Toko.java

--- Saraswati/src/saraswati/Toko.java
+++ Saraswati/src/saraswati/Toko.java
@@ -2,4 +2,51 @@ package saraswati;
 public class Toko {
     private int code;
     private String contact, address, state, postalCode, country;
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
