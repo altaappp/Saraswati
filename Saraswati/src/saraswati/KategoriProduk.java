e93d994be5073ffc62a405a78f235c741779f291
Altaff <121285212+altaappp@users.noreply.github.com>
2023-11-10 20:56:15 +0700

    Update KategoriProduk.java

--- Saraswati/src/saraswati/KategoriProduk.java
+++ Saraswati/src/saraswati/KategoriProduk.java
@@ -2,4 +2,19 @@ package saraswati;
 public class KategoriProduk extends Produk {
     private int id;
     private String name;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
