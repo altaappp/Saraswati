0fd305b11417c11d58dcf9373d715d702e4f54f2
Altaff <121285212+altaappp@users.noreply.github.com>
2023-11-10 20:57:46 +0700

    Update Produk.java

--- Saraswati/src/saraswati/Produk.java
+++ Saraswati/src/saraswati/Produk.java
@@ -3,4 +3,43 @@ public class Produk {
     private int id, scale;
     private String name, vendor;
     private double price;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScale() {
+        return scale;
+    }
+
+    public void setScale(int scale) {
+        this.scale = scale;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
