6f0ee226f664027444d9cf39769152336ca4990d
Altaff <121285212+altaappp@users.noreply.github.com>
2023-11-10 20:56:41 +0700

    Update Order.java

--- Saraswati/src/saraswati/Order.java
+++ Saraswati/src/saraswati/Order.java
@@ -3,4 +3,51 @@ public class Order {
     private int id, state;
     private String comments;
     private Date createDate, requestDate, shipmentDate;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Date getShipmentDate() {
+        return shipmentDate;
+    }
+
+    public void setShipmentDate(Date shipmentDate) {
+        this.shipmentDate = shipmentDate;
+    }
 }
