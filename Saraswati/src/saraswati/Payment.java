22beb3dc02fd8eb0ce528db9cfea363ee120e546
Altaff <121285212+altaappp@users.noreply.github.com>
2023-11-10 20:57:04 +0700

    Update Payment.java

--- Saraswati/src/saraswati/Payment.java
+++ Saraswati/src/saraswati/Payment.java
@@ -3,4 +3,27 @@ public class Payment {
     private int id;
     private Date createDate;
     private double amount;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
